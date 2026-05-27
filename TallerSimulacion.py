505de4236ddbb1612975c3728cf765078acfad81
Irving2355 <it235597@gmail.com>
2026-05-26 18:11:53 -0600

    Dia 2 taller

--- TallerSimulacion.py
+++ TallerSimulacion.py
@@ -21,6 +21,11 @@ FPS = 60
 GRAVEDAD = 0.5
 ITERACIONES = 5
 
+PUNTO_LANZAMIENTO = pygame.Vector2(100, 450) 
+RADIO_PROYECTIL = 10
+FACTOR_DISPARO = 0.18
+RADIO_IMPACTO = 18
+
 pantalla = pygame.display.set_mode((ANCHO, ALTO))
 reloj = pygame.time.Clock()
 
@@ -28,9 +33,15 @@ particulas = []
 
 ligaduras = []
 
+proyectiles = []
+
 particula_seleccionada = None
 
 simualacion_activa = False
+
+arrastrando_disparo = False
+
+pos_mouse_disparo = pygame.Vector2(PUNTO_LANZAMIENTO)
 
 '''class Particula:
     def __init__(self,x,y):
@@ -98,6 +109,29 @@ class Ligadura:
     def dibujar(self, pantalla):
         pygame.draw.line(pantalla, GRIS, self.p1.pos, self.p2.pos, 2) 
 
+class Proyectil:
+    def __init__(self, pos, velocidad):
+        self.pos = pygame.Vector2(pos)
+        self.vel = pygame.Vector2(velocidad)
+        self.radio = RADIO_PROYECTIL
+        self.activo = True
+    
+    def actualizar(self):
+        if not self.activo:
+            return
+        #aplicamos euler
+        self.vel.y += GRAVEDAD
+        self.pos += self.vel
+        
+        if self.pos.x > ANCHO + 100 or self.pos.y > ALTO + 100:
+            self.activo = False
+        
+        if self.pos.x < -100 or self.pos.y < -100:
+            self.activo = False
+        
+    def dibujar(self, pantalla):
+        if self.activo:
+            pygame.draw.circle(pantalla, ROJO, self.pos, self.radio)
 
 
 while True:
@@ -113,31 +147,52 @@ while True:
             pos_mouse = pygame.Vector2(pygame.mouse.get_pos())
             
             if evento.button == 1:
-                particula_seleccionada = None 
-                for p in particulas:
-                    if(p.pos - pos_mouse).length() < 10:
-                        particula_seleccionada = p
-                        break
                 
-                if particula_seleccionada is None:
-                    particulas.append(Particula(pos_mouse.x, pos_mouse.y))
+                if (pos_mouse - PUNTO_LANZAMIENTO).length() < 30:
+                    arrastrando_disparo = True
+                    pos_mouse_disparo = pos_mouse
+                else:
+                    particula_seleccionada = None 
+                    for p in particulas:
+                        if(p.pos - pos_mouse).length() < 10:
+                            particula_seleccionada = p
+                            break
+                
+                    if particula_seleccionada is None:
+                        particulas.append(Particula(pos_mouse.x, pos_mouse.y))
             
             if evento.button == 3:
                 for p in particulas:
                     if(p.pos - pos_mouse).length() < 10:
                         p.fija = not p.fija
                         break 
+            
+            if evento.type == pygame.MOUSEMOTION:
+                if arrastrando_disparo:
+                    pos_mouse_disparo = pygame.Vector2(pygame.mouse.get_pos()) 
         #fin if de mousebuttondown
         
         if evento.type == pygame.MOUSEBUTTONUP:
-            if evento.button == 1 and particula_seleccionada is not None:
-                pos_mouse = pygame.Vector2(pygame.mouse.get_pos()) 
-                
-                for p in particulas:
-                    if(p.pos - pos_mouse).length() < 10 and p != particula_seleccionada:
-                        ligaduras.append(Ligadura(particula_seleccionada, p))
-                        break
-                particula_seleccionada = None
+            
+            if evento.button == 1:
+                if arrastrando_disparo:
+                    pos_mouse = pygame.Vector2(pygame.mouse.get_pos())
+                    
+                    velocidad_inicial = (PUNTO_LANZAMIENTO - pos_mouse) * FACTOR_DISPARO
+                    
+                    proyectiles.append(Proyectil(PUNTO_LANZAMIENTO, velocidad_inicial))
+                    
+                    arrastrando_disparo = False
+                    pos_mouse_disparo = pygame.Vector2(PUNTO_LANZAMIENTO) 
+            
+                elif particula_seleccionada is not None:
+                    pos_mouse = pygame.Vector2(pygame.mouse.get_pos()) 
+                    
+                    for p in particulas:
+                        if(p.pos - pos_mouse).length() < 10 and p != particula_seleccionada:
+                            ligaduras.append(Ligadura(particula_seleccionada, p))
+                            break
+                    particula_seleccionada = None
         
         if evento.type == pygame.KEYDOWN:
             if evento.key == pygame.K_SPACE:
@@ -158,10 +213,41 @@ while True:
             for l in ligaduras:
                 l.aplicar()
     
+    for proyectil in proyectiles:
+        proyectil.actualizar()
+    
+    #verificamos si choco con una particula
+    for proyectil in proyectiles:
+        if not proyectil.activo:
+            continue
+        
+        for particula in particulas[:]:
+            distancia = (proyectil.pos - particula.pos).length()
+            if distancia < RADIO_IMPACTO:
+                #if not particula.fija:
+                    particulas.remove(particula)
+                    ligaduras = [
+                        l for l in ligaduras
+                        if l.p1 != particula and l.p2 != particula
+                    ]
+                    proyectil.activo = False
+                    break
+    
     for l in ligaduras:
         l.dibujar(pantalla)
     
     for p in particulas:
         p.dibujar(pantalla)
+    
+    pygame.draw.circle(pantalla, ROJO, PUNTO_LANZAMIENTO, 8)
+    
+    if arrastrando_disparo:
+        pygame.draw.line(pantalla, BLANCO, PUNTO_LANZAMIENTO,pos_mouse_disparo, 2)
+        pygame.draw.circle(pantalla, BLANCO, pos_mouse_disparo,RADIO_PROYECTIL) 
+    
+    for proyectil in proyectiles:
+        proyectil.dibujar(pantalla)
+        
+    proyectiles = [p for p in proyectiles if p.activo]
     
     pygame.display.flip()
